2c4809c2ba87bbd326b42e5afaf79922d0f6d885
deenakamal <deenah.kamal@gmail.com>
2024-02-11 14:18:39 -0800

    edit

--- models/amenity.py
+++ models/amenity.py
@@ -6,3 +6,7 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """ Class Amenity """
     name = ""
+
+        def __init__(self, *args, **kwargs):
+        """init"""
+        super().__init__(*args, **kwargs)
