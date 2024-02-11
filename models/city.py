987e94ebd8505e6e577eefbb6ba23e1ed85b5e45
deenakamal <deenah.kamal@gmail.com>
2024-02-11 14:22:04 -0800

    edit

--- models/city.py
+++ models/city.py
@@ -7,3 +7,7 @@ class City(BaseModel):
     """ Class City """
     state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init the Amenity object"""
+        super().__init__(*args, **kwargs)
