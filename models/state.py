13e8d834bc3b83f3b6d43a4a4a13ed87b47a730d
deenakamal <deenah.kamal@gmail.com>
2024-02-11 14:24:49 -0800

    edit models diles

--- models/state.py
+++ models/state.py
@@ -6,3 +6,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ Class State """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init"""
+        super().__init__(*args, **kwargs)
